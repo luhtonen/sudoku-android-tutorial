f1ffefbc7037c2042b1e7ae2a822610379668a22
Eduard Luhtonen <luhtonen@gmail.com>
2012-06-26 15:37:49 +0300

    initial version

---
+++ Sudoku/src/com/infrolog/elu/android/Sudoku.java
@@ -0,0 +1,13 @@
+package com.infrolog.elu.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Sudoku extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
