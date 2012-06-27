4d063d1f3ce0441be73730546a2928b5cf7ce1a7
Eduard Luhtonen <luhtonen@gmail.com>
2012-06-27 18:55:32 +0300

    Settings menu added

--- Sudoku/src/com/infrolog/elu/android/Sudoku.java
+++ Sudoku/src/com/infrolog/elu/android/Sudoku.java
@@ -3,6 +3,9 @@ package com.infrolog.elu.android;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 
@@ -30,5 +33,23 @@ public class Sudoku extends Activity implements OnClickListener {
 				startActivity(i);
 				break;
 		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId()) {
+			case R.id.settings:
+				startActivity(new Intent(this, Prefs.class));
+				return true;
+		}
+		return false;
 	}
 }
