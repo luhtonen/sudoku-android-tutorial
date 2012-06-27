c9cde5049d1a567f57792993e064281d62aa240b
Eduard Luhtonen <luhtonen@gmail.com>
2012-06-27 18:30:31 +0300

    About screen implementation

---
+++ Sudoku/src/com/infrolog/elu/android/About.java
@@ -0,0 +1,13 @@
+package com.infrolog.elu.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class About extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about);
+	}
+}
--- Sudoku/src/com/infrolog/elu/android/Sudoku.java
+++ Sudoku/src/com/infrolog/elu/android/Sudoku.java
@@ -1,13 +1,34 @@
 package com.infrolog.elu.android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class Sudoku extends Activity {
+public class Sudoku extends Activity implements OnClickListener {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+     // Set up click listeners for all the buttons
+        View continueButton = findViewById(R.id.continue_button);
+        continueButton.setOnClickListener(this);
+        View newButton = findViewById(R.id.new_button);
+        newButton.setOnClickListener(this);
+        View aboutButton = findViewById(R.id.about_button);
+        aboutButton.setOnClickListener(this);
+        View exitButton = findViewById(R.id.exit_button);
+        exitButton.setOnClickListener(this);
     }
+
+	public void onClick(View v) {
+		switch(v.getId()) {
+			case R.id.about_button:
+				Intent i = new Intent(this, About.class);
+				startActivity(i);
+				break;
+		}
+	}
 }
