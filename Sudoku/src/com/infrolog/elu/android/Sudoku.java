ed1472e3dedf874b4958891cafb7c611523fff3c
Eduard Luhtonen <luhtonen@gmail.com>
2012-06-30 19:47:10 +0300

    game logic initial implementation

--- Sudoku/src/com/infrolog/elu/android/Sudoku.java
+++ Sudoku/src/com/infrolog/elu/android/Sudoku.java
@@ -79,6 +79,8 @@ public class Sudoku extends Activity implements OnClickListener {
 
 	private void startGame(int i) {
 		Log.d(TAG, "clicked on " + i);
-		// Start game here...
+		Intent intent = new Intent(this, Game.class);
+		intent.putExtra(Game.KEY_DIFFICULTY, i);
+		startActivity(intent);
 	}
 }
